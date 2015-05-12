07f2693686960b2f5fdcc1ae52e6b5f6e8756b86
Sam Saccone <samccone@gmail.com>
2015-05-12 17:28:25 -0400

    :truck: Fix incorrect spacing

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ function isVisible(elm) {
 
   return rect.bottom > 0 &&
     rect.right > 0 &&
-    rect.left < (window.innerWidth || document. documentElement.clientWidth) &&
-    rect.top < (window.innerHeight || document. documentElement.clientHeight);
+    rect.left < (window.innerWidth || document.documentElement.clientWidth) &&
+    rect.top < (window.innerHeight || document.documentElement.clientHeight);
 }
 
 function onScroll() {

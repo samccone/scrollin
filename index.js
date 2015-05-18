samccone/scrollin

commit e9e94314a99c79e550afef3753c05371f54b274d

:racehorse: Throttle checks.
Ensure that it is only on the trailing edge since we do not care about
the initial scroll, rather only when someone stops scrolling.

index.js
@@ -1,4 +1,5 @@
 var tracking = [];
+const throttle = require('lodash.throttle');
 
 function isVisible(elm) {
   let rect = elm.getBoundingClientRect();
@@ -45,7 +46,7 @@ function track(elm, options) {
   window.requestAnimationFrame(() => _trackNewElement(elm, options));
 
   if (tracking.length === 0) {
-    window.addEventListener('scroll', _onScroll);
+    window.addEventListener('scroll', throttle(_onScroll, 100, {leading: false}));
   }
 }
 

commit 907ff177fbb02492d5236becf46013ab678bc060

:card_index: Fix leaky event registration
This arose because of the change introduced in
e9e94314a99c79e550afef3753c05371f54b274d

Fixes #6
Thanks @ianmstew

index.js
@@ -1,6 +1,8 @@
 var tracking = [];
 const throttle = require('lodash.throttle');
 
+const _onScroll = throttle(() => window.requestAnimationFrame(checkForVisibleElements), 100, {leading: false});
+
 function isVisible(elm) {
   let rect = elm.getBoundingClientRect();
 
@@ -8,10 +10,6 @@ function isVisible(elm) {
     rect.right > 0 &&
     rect.left < (window.innerWidth || document.documentElement.clientWidth) &&
     rect.top < (window.innerHeight || document.documentElement.clientHeight);
-}
-
-function _onScroll() {
-  window.requestAnimationFrame(checkForVisibleElements);
 }
 
 function _handleVisible(elm, options) {
@@ -46,7 +44,7 @@ function track(elm, options) {
   window.requestAnimationFrame(() => _trackNewElement(elm, options));
 
   if (tracking.length === 0) {
-    window.addEventListener('scroll', throttle(_onScroll, 100, {leading: false}));
+    window.addEventListener('scroll', _onScroll);
   }
 }
 